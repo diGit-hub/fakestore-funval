8cff038a3d75b637d1294beb7923d2baf6827abc
diGit-hub <gabrielsud.m.s@gmail.com>
2026-03-24 09:46:38 -0300

    feat: enhance ProductCard styling, update Home layout, and integrate Tailwind CSS

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,25 @@
 export default function ProductCard({ product }) {
     if (!product) return null;
-    
+
     const { id, title, price, image, category } = product;
     return (
-        <div key={id}>
-            <img src={image} alt={title} />
-            <p>{category}</p>
-            <p>{title}</p>
-            <p>{price}</p>
+        <div key={id} className="flex flex-col p-4 bg-white rounded-2xl shadow-sm h-full">
+            <div className="w-full h-48 mb-4">
+                <img
+                    src={image}
+                    alt={title}
+                    className="w-full h-full object-contain"
+                />
+            </div>
+            <div className="flex flex-col flex-1">
+                <p className="text-xs text-primary uppercase">{category}</p>
+                <h3 className="font-semibold text-gray-800 mt-1 line-clamp-2" title={title}>
+                    {title}
+                </h3>
+                <p className="text-gray-800 font-bold text-lg mt-auto pt-2">
+                    ${price.toFixed(2)}
+                </p>
+            </div>
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,17 +3,17 @@ import ProductCard from "../components/ProductCard";
 import { useFetchProducts } from "../hooks/useFetchProducts";
 
 export default function Home() {
-    const { products, loading, error} = useFetchProducts();
+    const { products, loading, error } = useFetchProducts();
     return (
         <>
             <Header></Header>
-            <main>
+            <main className="bg-background">
                 <section className="flex justify-center items-center h-120 shadow">
                     <p>Hero Section</p>
                 </section>
                 <section>
-                    <div className="grid">
-                        <p>Products</p>
+                    <p>Products</p>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 px-48">
                         {products.map((product) => (
                             <ProductCard key={product.id} product={product}></ProductCard>
                         ))}
