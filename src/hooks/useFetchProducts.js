0082b9e6f2770197cbfbd5b372aeec5d1200293d
diGit-hub <gabrielsud.m.s@gmail.com>
2026-03-24 04:30:43 -0300

    feat: implement useFetchProducts hook and add Footer component

--- src/hooks/useFetchProducts.js
+++ src/hooks/useFetchProducts.js
@@ -1,3 +1,25 @@
-export default function useFetchProducts() {
-  
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export const useFetchProducts = () => {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('https://fakestoreapi.com/products');
+                setProducts(response.data);
+            }
+            catch (err) {
+                setError("Couldn't load products");
+            }
+            finally {
+                setLoading(false);
+            }
+        }
+        fetchData();
+    }, [])
+    return {products, loading, error}
 }
