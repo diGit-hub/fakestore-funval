1bae7e8ed06e4237e6fe314eab3a1964f406dcc7
diGit-hub <gabrielsud.m.s@gmail.com>
2026-03-24 04:38:32 -0300

    feat: restructure App component and reintroduce Header in Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Header from './components/Header'
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 
 function App() {
   return (
-    <>
-      <Header></Header>
-    </>
+    <HashRouter>
+        <Routes>
+            <Route index path='/' element={<Home/>}/>
+        </Routes>
+    </HashRouter>
   )
 }
 
