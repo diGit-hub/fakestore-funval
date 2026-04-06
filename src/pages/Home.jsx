524f3ff80d495f6312c16d3f3a40bea592236e74
Gabriel Mercês <67835914+diGit-hub@users.noreply.github.com>
2026-04-06 07:49:07 -0300

    feat: add message for no products found in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,6 +26,9 @@ export default function Home() {
                 </section>
                 <section>
                     <p>Products</p>
+                    {filteredProducts.length === 0 && (
+                        <p className="text-center text-gray-500 py-8">No products found</p>
+                    )}
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 px-48">
                         {filteredProducts.map((product) => (
                             <ProductCard key={product.id} product={product}></ProductCard>
